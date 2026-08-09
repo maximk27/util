fce342e445dd4ddfce79c1ae738b8d72a940a09b
maximk27 <maximk@umich.edu>
2026-08-09 14:59:08 -0700

    use simpler impl

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### What is this
 Utils!
+
+
+## todo
+[euler tour](https://usaco.guide/gold/tree-euler)
---
+++ cpp/bit.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class BIT {
+    vector<int64_t> tree;
+
+private:
+    int GetLSB(int val) {
+        return val & (-val);
+    }
+
+public:
+    BIT(int n) : tree(n + 1, 0) {
+    }
+
+    // [0, k]
+    int64_t Sum(int k) {
+        k++;
+        int64_t sum = 0;
+        while (k >= 1) {
+            sum += tree[k];
+            k -= GetLSB(k);
+        }
+        return sum;
+    }
+
+    // [l, r]
+    int64_t Sum(int l, int r) {
+        return Sum(r + 1) - Sum(l);
+    }
+
+    void Add(int k, int64_t d) {
+        k++;
+        while (k < tree.size()) {
+            tree[k] += d;
+            k += GetLSB(k);
+        }
+    }
+};
