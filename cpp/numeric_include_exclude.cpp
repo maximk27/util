de535c20964502fc583ba17091a6232882116dbf
maximk27 <maximk@umich.edu>
2026-08-21 23:11:30 -0700

    refactor

--- cpp/numeric_include_exclude.cpp
+++ cpp/numeric_include_exclude.cpp
@@ -1,29 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int64_t CountDivAtLeastOne(int64_t N, const vector<int64_t> &A) {
-    if (N <= 0 || A.empty())
-        return 0;
-
-    int K = static_cast<int>(A.size());
+int64_t CountLTE(int64_t bound, vector<int> &nums) {
+    int n = nums.size();
     int64_t total = 0;
-    int num_subsets = 1 << K;
-
-    for (int mask = 1; mask < num_subsets; ++mask) {
+    int m = 1 << n;
+    for (int mask = 1; mask < m; mask++) {
         int64_t current_lcm = 1;
-        for (int i = 0; i < K; ++i) {
+        for (int i = 0; i < n; ++i) {
             if (mask & (1 << i)) {
-                current_lcm = lcm(current_lcm, A[i]);
+                current_lcm = lcm(current_lcm, nums[i]);
             }
         }
 
-        int64_t count = N / current_lcm;
+        int64_t count = bound / current_lcm;
         if (__builtin_popcount(mask) % 2 == 1) {
             total += count;
         } else {
             total -= count;
         }
     }
-
     return total;
 }
