18e1a5a2c6b74170328288c140a4645bfd4e8263
maximk27 <maximk@umich.edu>
2026-09-14 15:01:36 -0400

    cannot mod exponent and more concise impl

--- cpp/combinatorics.cpp
+++ cpp/combinatorics.cpp
@@ -7,18 +7,17 @@ using namespace std;
 // SINGLE USE
 // ========================================================================
 int64_t Pow(int64_t base, int64_t exp, int64_t mod) {
-    // a^b = (a^2)^(b/2)
     base %= mod;
-    exp %= mod;
     int64_t res = 1;
-    while (exp > 0) {
-        if (exp % 2 == 0) {
-            base = (base * base) % mod;
-            exp /= 2;
-        } else {
+    while (exp >= 1) {
+        if (exp % 2 == 1) {
             res = (res * base) % mod;
             exp--;
+            // 1 -> 0
         }
+        // 0
+        exp >>= 1;
+        base = (base * base) % mod;
     }
     return res;
 }
